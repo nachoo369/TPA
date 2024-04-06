c259ff5f9612b9a2ff5274e4fc6d6b9e42120b69
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2024-04-06 16:16:31 -0300

    Tamaño ventanas

--- OneDrive/Escritorio/TPA-T/info_hotel.py
+++ OneDrive/Escritorio/TPA-T/info_hotel.py
@@ -13,10 +13,11 @@ class HotelInfoWidget(QWidget):
         hotel_info_group = QGroupBox('Información del Hotel')
         hotel_info_layout = QVBoxLayout()
         hotel_info_label = QLabel(
-            "El Hotel CTCh es un espacio de descanso, relajación y comodidad, con instalaciones basadas en la naturaleza del entorno, pero con todo el lujo y privacidad de un establecimiento de cinco estrellas.")
+            "El Hotel CTCh es un espacio de descanso, relajación y comodidad,\ncon instalaciones basadas en la naturaleza del entorno,\npero con todo el lujo y privacidad de un\nestablecimiento de cinco estrellas.")
         hotel_info_layout.addWidget(hotel_info_label)
         hotel_info_group.setLayout(hotel_info_layout)
 
         layout.addWidget(hotel_info_group)
 
         self.setLayout(layout)
+        self.setGeometry(100,100,400,400)
